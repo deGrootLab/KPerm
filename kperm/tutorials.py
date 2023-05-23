074adecc308689d4a4b06136661a0d3be0947f55
Chun Kei (Tom) Lam <chun-kei.lam@mpinat.mpg.de>
2023-05-23 19:03:21 +0200

    separate tutorial materials from release

---
+++ kperm/tutorials.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import urllib.request
+
+
+def download_charge_scaling():
+    data_root_url = (
+        "https://github.com/deGrootLab/KPerm/"
+        "raw/main/tutorials/charge-scaling/"
+    )
+    pdb = Path("MthK.pdb")
+    if pdb.is_file():
+        print(str(pdb) + " exists, will not download it.")
+    else:
+        urllib.request.urlretrieve(data_root_url + "MthK.pdb", pdb)
+
+    for i in range(3):
+        name = f"traj-{i}"
+        Path(name).mkdir(exist_ok=True)
+
+        traj = Path(name, name + ".xtc")
+        if traj.is_file():
+            print(str(traj) + " exists, will not download it.")
+        else:
+            urllib.request.urlretrieve(
+                data_root_url + f"{name}/{name}.xtc", traj)
+
+    print("Finished downloading MthK.pdb and trajectories.")
